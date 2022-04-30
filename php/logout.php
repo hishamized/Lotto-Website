15a7c37e9f8a22c1f1b77dc4e45ec170dbbb5538
hishamized <mirhashim2001@gmail.com>
2022-04-30 19:10:28 +0530

    Logout Functionality Added

--- php/logout.php
+++ php/logout.php
@@ -1,8 +1,7 @@
 <?php
- session_start();
-
- $_SESSION = array();
- session_destroy();
- header("lcoation: index.php");
-
+    if (!isset($_SESSION)) { session_start(); }
+    $_SESSION = array(); 
+    session_destroy(); 
+    header("Location: login.php"); // Or wherever you want to redirect
+    exit();
 ?>

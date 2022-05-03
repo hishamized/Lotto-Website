f813eadf1f1ab56635e2dffb9d0a19637e7734b1
hishamized <mirhashim2001@gmail.com>
2022-05-03 21:04:50 +0530

    Real Time Countdown Functionality added to 5 games

--- game_time.php
+++ game_time.php
@@ -92,22 +92,27 @@
         <div id="game1" class="game game1">
             <h3>Game 1</h3>
             <p>Write game code here...</p>
+            <div>Game Closes In  <span class="two_hours" id="time1">Time Left</span></div>
         </div>
         <div id="game2" class="game game2">
             <h3>Game 2</h3>
             <p>Write game code here...</p>
+            <div>Game Closes In  <span class="two_hours" id="time2">Time Left</span></div>
         </div>
         <div id="game3" class="game game3">
             <h3>Game 3</h3>
             <p>Write game code here...</p>
+            <div>Game Closes In  <span class="two_hours" id="time3">Time Left</span></div>
         </div>
         <div id="game4" class="game game4">
             <h3>Game 4</h3>
             <p>Write game code here...</p>
+            <div>Game Closes In  <span class="two_hours" id="time4">Time Left</span></div>
         </div>
         <div id="game5" class="game game5">
             <h3>Game 5</h3>
             <p>Write game code here...</p>
+            <div>Game Closes In  <span class="two_hours" id="time5">Time Left</span></div>
         </div>
     </section>
     <div id="message">No games available at this time. Please come back later.</div>
@@ -116,6 +121,9 @@
 
       </h3>
    </div>
+
+   
+   
 
     <script>
         var today = new Date();
@@ -133,30 +141,200 @@
              game1.style.display = "flex";
              const message = document.getElementById("message");
              message.style.display = "none";
+
+             var hours_now = today.getHours();
+             var minutes_now = today.getMinutes();
+             var seconds_now = today.getSeconds();
+
+             var hours_left = 22 - hours_now -1;
+             var minutes_left = (60 - minutes_now) + (hours_left*60);
+             var seconds_left = (60 - seconds_now) + minutes_left*60 ;
+
+             function startTimer(duration, display) {
+            var timer = duration, hours, minutes, seconds;
+            setInterval(function () {
+                hours = parseInt( (timer)/3600, 10);
+                //Separates the hours from minutes and returns the remaining minutes
+                minutes = parseInt((timer / 60)%60, 10); 
+                seconds = parseInt(timer % 60, 10);
+
+                hours = hours < 10 ? "0" + hours : hours;
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                display.textContent = hours + ":" + minutes + ":" + seconds;
+
+                if (--timer < 0) {
+                    timer = duration;
+                }
+            }, 1000);
+        }
+
+            window.onload = function () {
+                var countDownTIme = seconds_left,
+                    display = document.querySelector("#time5");
+                startTimer(countDownTIme, display);
+            };
          }
          if(time_value >= 11 && time_value <= 13){
              const game2 = document.getElementById("game2");
              game2.style.display = "flex";
              const message = document.getElementById("message");
              message.style.display = "none";
+
+             var hours_now = today.getHours();
+             var minutes_now = today.getMinutes();
+             var seconds_now = today.getSeconds();
+
+             var hours_left = 22 - hours_now -1;
+             var minutes_left = (60 - minutes_now) + (hours_left*60);
+             var seconds_left = (60 - seconds_now) + minutes_left*60 ;
+
+             function startTimer(duration, display) {
+            var timer = duration, hours, minutes, seconds;
+            setInterval(function () {
+                hours = parseInt( (timer)/3600, 10);
+                //Separates the hours from minutes and returns the remaining minutes
+                minutes = parseInt((timer / 60)%60, 10); 
+                seconds = parseInt(timer % 60, 10);
+
+                hours = hours < 10 ? "0" + hours : hours;
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                display.textContent = hours + ":" + minutes + ":" + seconds;
+
+                if (--timer < 0) {
+                    timer = duration;
+                }
+            }, 1000);
+        }
+
+            window.onload = function () {
+                var countDownTIme = seconds_left,
+                    display = document.querySelector("#time5");
+                startTimer(countDownTIme, display);
+            };
          }
          if(time_value >=14  && time_value <= 16){
              const game3 = document.getElementById("game3");
              game3.style.display = "flex";
              const message = document.getElementById("message");
              message.style.display = "none";
+
+             var hours_now = today.getHours();
+             var minutes_now = today.getMinutes();
+             var seconds_now = today.getSeconds();
+
+             var hours_left = 22 - hours_now -1;
+             var minutes_left = (60 - minutes_now) + (hours_left*60);
+             var seconds_left = (60 - seconds_now) + minutes_left*60 ;
+
+             function startTimer(duration, display) {
+            var timer = duration, hours, minutes, seconds;
+            setInterval(function () {
+                hours = parseInt( (timer)/3600, 10);
+                //Separates the hours from minutes and returns the remaining minutes
+                minutes = parseInt((timer / 60)%60, 10); 
+                seconds = parseInt(timer % 60, 10);
+
+                hours = hours < 10 ? "0" + hours : hours;
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                display.textContent = hours + ":" + minutes + ":" + seconds;
+
+                if (--timer < 0) {
+                    timer = duration;
+                }
+            }, 1000);
+        }
+
+            window.onload = function () {
+                var countDownTIme = seconds_left,
+                    display = document.querySelector("#time5");
+                startTimer(countDownTIme, display);
+            };
          }
          if(time_value >=17  && time_value <= 19){
              const game4 = document.getElementById("game4");
              game4.style.display = "flex";
              const message = document.getElementById("message");
              message.style.display = "none";
+             var hours_now = today.getHours();
+             var minutes_now = today.getMinutes();
+             var seconds_now = today.getSeconds();
+
+             var hours_left = 22 - hours_now -1;
+             var minutes_left = (60 - minutes_now) + (hours_left*60);
+             var seconds_left = (60 - seconds_now) + minutes_left*60 ;
+
+             function startTimer(duration, display) {
+            var timer = duration, hours, minutes, seconds;
+            setInterval(function () {
+                hours = parseInt( (timer)/3600, 10);
+                //Separates the hours from minutes and returns the remaining minutes
+                minutes = parseInt((timer / 60)%60, 10); 
+                seconds = parseInt(timer % 60, 10);
+
+                hours = hours < 10 ? "0" + hours : hours;
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                display.textContent = hours + ":" + minutes + ":" + seconds;
+
+                if (--timer < 0) {
+                    timer = duration;
+                }
+            }, 1000);
+        }
+
+            window.onload = function () {
+                var countDownTIme = seconds_left,
+                    display = document.querySelector("#time5");
+                startTimer(countDownTIme, display);
+            };
          }
          if(time_value >=20  && time_value <= 22){
              const game5 = document.getElementById("game5");
              game5.style.display = "flex";
              const message = document.getElementById("message");
              message.style.display = "none";
+
+
+             var hours_now = today.getHours();
+             var minutes_now = today.getMinutes();
+             var seconds_now = today.getSeconds();
+
+             var hours_left = 22 - hours_now -1;
+             var minutes_left = (60 - minutes_now) + (hours_left*60);
+             var seconds_left = (60 - seconds_now) + minutes_left*60 ;
+
+             function startTimer(duration, display) {
+            var timer = duration, hours, minutes, seconds;
+            setInterval(function () {
+                hours = parseInt( (timer)/3600, 10);
+                //Separates the hours from minutes and returns the remaining minutes
+                minutes = parseInt((timer / 60)%60, 10); 
+                seconds = parseInt(timer % 60, 10);
+
+                hours = hours < 10 ? "0" + hours : hours;
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                display.textContent = hours + ":" + minutes + ":" + seconds;
+
+                if (--timer < 0) {
+                    timer = duration;
+                }
+            }, 1000);
+        }
+
+            window.onload = function () {
+                var countDownTIme = seconds_left,
+                    display = document.querySelector("#time5");
+                startTimer(countDownTIme, display);
+            };
          }
     </script>
 </body>
